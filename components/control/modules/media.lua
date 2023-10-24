406e00d8b58233e130e2c1e2fbf977affc70425e
Daniel Liu <daniel17liu@gmail.com>
2023-10-23 23:13:35 -0700

    media only scroll on hover

--- components/control/modules/media.lua
+++ components/control/modules/media.lua
@@ -18,22 +18,41 @@ local function create_widget()
         clip_shape = function(cr, width, height)
             gears.shape.rounded_rect(cr, width, height, 8)
         end,
-        widget = wibox.widget.imagebox,
+        widget = wibox.widget.imagebox
     })
 
-    local title_widget = wibox.widget({
+    local title_text = wibox.widget({
         markup = "Playing",
         align = "left",
         valign = "center",
-        widget = wibox.widget.textbox,
+        widget = wibox.widget.textbox
     })
 
-    local artist_widget = wibox.widget({
+    local title_widget = wibox.widget({
+        layout = wibox.container.scroll.horizontal,
+        step_function = wibox.container.scroll.step_functions.waiting_nonlinear_back_and_forth,
+        fps = 60,
+        speed = 75,
+        title_text
+    })
+
+    title_widget:pause()
+
+    local artist_text = wibox.widget({
         markup = "Nothing",
         align = "left",
         valign = "center",
-        widget = wibox.widget.textbox,
+        widget = wibox.widget.textbox
     })
+
+    local artist_widget = wibox.widget({
+        layout = wibox.container.scroll.horizontal,
+        step_function = wibox.container.scroll.step_functions.waiting_nonlinear_back_and_forth,
+        fps = 60,
+        speed = 75,
+        artist_text
+    })
+    artist_widget:pause()
 
     local toggle_button = wibox.widget({
         image = beautiful.icon_play,
@@ -41,7 +60,7 @@ local function create_widget()
         forced_height = beautiful.panel_button_icon_size,
         forced_width = beautiful.panel_button_icon_size,
         align = "center",
-        valign = "center",
+        valign = "center"
     })
 
     local prev_button = wibox.widget({
@@ -50,7 +69,7 @@ local function create_widget()
         forced_height = beautiful.panel_button_icon_size,
         forced_width = beautiful.panel_button_icon_size,
         align = "center",
-        valign = "center",
+        valign = "center"
     })
 
     local next_button = wibox.widget({
@@ -59,7 +78,7 @@ local function create_widget()
         forced_height = beautiful.panel_button_icon_size,
         forced_width = beautiful.panel_button_icon_size,
         align = "center",
-        valign = "center",
+        valign = "center"
     })
 
     toggle_button:buttons(gears.table.join(awful.button({}, 1, function()
@@ -79,71 +98,69 @@ local function create_widget()
             {
                 art,
                 margins = beautiful.panel_internal_margin,
-                layout = wibox.container.margin,
+                layout = wibox.container.margin
             },
             {
                 {
                     {
-                        {
-                            layout = wibox.container.scroll.horizontal,
-                            step_function = wibox.container.scroll.step_functions.waiting_nonlinear_back_and_forth,
-                            fps = 60,
-                            speed = 75,
-                            artist_widget,
-                        },
+                        artist_widget,
                         fg = beautiful.panel_fg,
-                        widget = wibox.container.background,
+                        widget = wibox.container.background
                     },
                     {
-                        {
-                            layout = wibox.container.scroll.horizontal,
-                            step_function = wibox.container.scroll.step_functions.waiting_nonlinear_back_and_forth,
-                            fps = 60,
-                            speed = 75,
-                            title_widget,
-                        },
+                        title_widget,
                         fg = beautiful.panel_fg,
-                        widget = wibox.container.background,
+                        widget = wibox.container.background
                     },
-                    layout = wibox.layout.flex.vertical,
+                    layout = wibox.layout.flex.vertical
                 },
                 right = beautiful.panel_internal_margin,
                 top = beautiful.panel_internal_margin,
                 bottom = beautiful.panel_internal_margin,
-                layout = wibox.container.margin,
+                layout = wibox.container.margin
             },
             {
                 {
                     {
                         prev_button,
                         margins = beautiful.medium_space,
-                        layout = wibox.container.margin,
+                        layout = wibox.container.margin
                     },
                     {
                         toggle_button,
                         margins = beautiful.medium_space,
-                        layout = wibox.container.margin,
+                        layout = wibox.container.margin
                     },
                     {
                         next_button,
                         margins = beautiful.medium_space,
-                        layout = wibox.container.margin,
+                        layout = wibox.container.margin
                     },
-                    layout = wibox.layout.flex.horizontal,
+                    layout = wibox.layout.flex.horizontal
                 },
                 margins = beautiful.panel_internal_margin,
-                layout = wibox.container.margin,
+                layout = wibox.container.margin
             },
-            layout = wibox.layout.align.horizontal,
+            layout = wibox.layout.align.horizontal
         },
         margins = beautiful.panel_internal_margin,
-        layout = wibox.container.margin,
+        layout = wibox.container.margin
     }), 0, 0)
+
+    media:connect_signal("mouse::enter", function()
+        title_widget:continue()
+        artist_widget:continue()
+    end)
+
+    media:connect_signal("mouse::leave", function()
+        title_widget:pause()
+        artist_widget:pause()
+    end)
 
     playerctl:connect_signal("metadata", function(_, title, artist, album_path, album, new, player_name)
         art:set_image(gears.surface.load_uncached(album_path))
-        title_widget:set_markup_silently(title)
-        artist_widget:set_markup_silently(artist)
+        title_text:set_markup_silently(title)
+        artist_text:set_markup_silently(artist)
     end)
 
     playerctl:connect_signal("playback_status", function(_, playing, player_name)
